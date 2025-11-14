2973e96401b4f9e62b1fbd4bcdd1b85d8f309d41
Nikitkakuzmin <134796013+Nikitkakuzmin@users.noreply.github.com>
2025-11-14 11:55:19 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,11 @@
 <?php
-// config.php
-
 return [
     // 1C endpoint для картинок
-    // типичный вид: http://10.20.20.37/hs/JP/JewPhoto/ или https://your-1c-server/hs/JP/JewPhoto/
-    // ВАЖНО: со слэшем в конце
-    'onec_url'      => 'http://YOUR-1C-SERVER/hs/JP/JewPhoto/',
+    'onec_url'      => 'http://srv-test02/hs/JP/JewPhoto/',
 
     // MinIO
-    'minio_endpoint' => 'http://10.20.20.37:9000',
+    'minio_endpoint' => 'http://10.20.20.37:9101',
     'minio_bucket'   => 'photo2',
-    'minio_prefix'   => 'test2/2023-06-15', // для теста можно зафиксировать дату
+    'minio_prefix'   => 'test2/2023-06-15', // для первого теста можно зафиксировать дату
     'minio_filename_pattern' => '{item_code}-w.webp',
 ];
